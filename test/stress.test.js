87e93b9c548e77e93a4e280c3e497ce333da6863
uzlopak <aras.abbasi@googlemail.com>
2022-09-28 13:18:13 +0200

    improve testing

--- test/stress.test.js
+++ test/stress.test.js
@@ -6,7 +6,7 @@ const autocannon = require('autocannon')
 const fastifyEarlyHints = require('../index')
 
 test('Should not add Early Hints', t => {
-  t.plan(5)
+  t.plan(8)
 
   const fastify = Fastify({ logger: false })
   fastify.register(fastifyEarlyHints)
@@ -25,9 +25,12 @@ test('Should not add Early Hints', t => {
       amount: 10000
     }, (err, result) => {
       t.error(err)
+      t.not(result['1xx'], 0)
+      t.not(result['2xx'], 0)
       t.equal(result['3xx'], 0)
       t.equal(result['4xx'], 0)
       t.equal(result['5xx'], 0)
+      t.strictSame(Object.keys(result.statusCodeStats), ['103', '200'])
       fastify.close()
     })
   })
