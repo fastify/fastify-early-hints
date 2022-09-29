f62305f8cc17bad43bb58af48147395691a916bf
uzlopak <aras.abbasi@googlemail.com>
2022-09-29 14:52:29 +0200

    add error handling for failing write to the socket

--- test/wrapWriteInPromise.test.js
+++ test/wrapWriteInPromise.test.js
@@ -6,18 +6,22 @@ const EarlyHintsWriteError = require('../lib/wrapWriteInPromise').EarlyHintsWrit
 
 test('wrapWriteInPromise: should resolve', async t => {
   t.plan(3)
-  await t.resolves(wrapWriteInPromise({ write: (value, encoding, cb) => {
-    t.same(value, 'value')
-    t.same(encoding, 'utf-8')
-    cb(null)
-  }}, 'value', 'utf-8'))
+  await t.resolves(wrapWriteInPromise({
+    write: (value, encoding, cb) => {
+      t.same(value, 'value')
+      t.same(encoding, 'utf-8')
+      cb(null)
+    }
+  }, 'value', 'utf-8'))
 })
 
 test('wrapWriteInPromise: should reject', async t => {
   t.plan(3)
-  await t.rejects(wrapWriteInPromise({ write: (value, encoding, cb) => {
-    t.same(value, 'value')
-    t.same(encoding, 'utf-8')
-    cb(new Error('something'))
-  }}, 'value', 'utf-8'), new EarlyHintsWriteError())
+  await t.rejects(wrapWriteInPromise({
+    write: (value, encoding, cb) => {
+      t.same(value, 'value')
+      t.same(encoding, 'utf-8')
+      cb(new Error('something'))
+    }
+  }, 'value', 'utf-8'), new EarlyHintsWriteError())
 })
