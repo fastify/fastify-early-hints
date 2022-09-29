9bca83c404a472fb7632a0df55ba323469ee5a49
KaKa <kaka@kakawebsitedemo.com>
2022-09-30 01:30:52 +0800

    test: ensure receive order

--- test/writeEarlyHints.test.js
+++ test/writeEarlyHints.test.js
@@ -4,6 +4,7 @@ const { test } = require('tap')
 const Fastify = require('fastify')
 const eh = require('../index')
 const { Client } = require('undici')
+const { setTimeout } = require('timers/promises')
 
 test('Should not add Early Hints', async (t) => {
   t.plan(1)
@@ -177,6 +178,100 @@ test('Should add Early Hints headers - array with same header', async (t) => {
   t.equal(Array.isArray(infos[0].headers.link), true)
   t.equal(infos[0].headers.link[0], '</style.css>; rel=preload; as=style')
   t.equal(infos[0].headers.link[1], '</script.js>; rel=preload; as=script')
+})
+
+test('Should add multiple Early Hints headers', async (t) => {
+  t.plan(7)
+  let _resolve
+  const promise = new Promise(function (resolve) {
+    _resolve = resolve
+  })
+  const payload = { hello: 'world' }
+
+  const fastify = Fastify()
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.register(eh)
+  fastify.get('/', async (request, reply) => {
+    await reply.writeEarlyHints({
+      'Content-Security-Policy': 'style-src: self;',
+      Link: '</style.css>; rel=preload; as=style'
+    })
+    await setTimeout(100)
+    await reply.writeEarlyHints([
+      { name: 'Link', value: '</image.png>; rel=preload; as=image' }
+    ])
+    await setTimeout(400)
+    return payload
+  })
+
+  await fastify.listen({ port: 0 })
+
+  const client = new Client(`http://localhost:${fastify.server.address().port}`, {
+    keepAliveTimeout: 10,
+    keepAliveMaxTimeout: 10
+  })
+  t.teardown(client.close.bind(client))
+
+  let headerCount = 0
+  client.dispatch({
+    method: 'GET',
+    path: '/'
+  }, {
+    onConnect () {
+      t.pass('connected')
+    },
+    onError (err) {
+      console.log(err)
+      t.fail('should not call')
+    },
+    onUpgrade () {
+      t.fail('should not call')
+    },
+    onHeaders (statusCode, headers) {
+      switch (headerCount) {
+        case 0: {
+          t.equal(statusCode, 103)
+          t.same(headers, [
+            Buffer.from('Content-Security-Policy'),
+            Buffer.from('style-src: self;'),
+            Buffer.from('Link'),
+            Buffer.from('</style.css>; rel=preload; as=style')
+          ])
+          break
+        }
+        case 1: {
+          t.equal(statusCode, 103)
+          t.same(headers, [
+            Buffer.from('Link'),
+            Buffer.from('</image.png>; rel=preload; as=image')
+          ])
+          break
+        }
+        case 2: {
+          t.equal(statusCode, 200)
+          break
+        }
+        default: {
+          t.fail('too many header')
+          break
+        }
+      }
+      headerCount++
+    },
+    onData () {
+      return true
+    },
+    onComplete () {
+      t.pass('completed')
+      _resolve()
+    },
+    onBodySent () {
+      t.fail('should not call')
+    }
+  })
+
+  await promise
 })
 
 test('Should throw when invalid input', async (t) => {
