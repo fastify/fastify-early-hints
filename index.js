d59d89ed0c83b191f3251d8efaa76572e42029cc
uzlopak <aras.abbasi@googlemail.com>
2022-11-04 16:16:14 +0100

    http2 support for discussion

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 
 const fp = require('fastify-plugin')
 const formatEntry = require('./lib/formatEntry')
-const CRLF = '\r\n'
-const EarlyHints = `HTTP/1.1 103 Early Hint${CRLF}`
+const EarlyHints = 'HTTP/1.1 103 Early Hint\r\n'
 
 function fastifyEarlyHints (fastify, opts, next) {
-  if (fastify.initialConfig.http2 === true) {
-    return next(Error('Early Hints cannot be used with a HTTP2 server.'))
-  }
+  const http2 = fastify.initialConfig.http2 || false
+  const separator = http2
+    ? ','
+    : '\r\n'
 
   const formatEntryOpts = {
-    warn: opts.warn
+    warn: opts.warn,
+    http2
   }
 
   function writeEarlyHints (headers) {
-    const reply = this
     let message = ''
     if (Array.isArray(headers)) {
       for (const nameValues of headers) {
         if (typeof nameValues === 'object' && typeof nameValues.name === 'string' && typeof nameValues.value === 'string') {
-          message += `${nameValues.name}: ${nameValues.value}${CRLF}`
+          message += `${nameValues.name}: ${nameValues.value}${separator}`
         } else {
           return Promise.reject(Error('"headers" expected to be name-value object'))
         }
@@ -29,50 +29,56 @@ function fastifyEarlyHints (fastify, opts, next) {
       for (const key of Object.keys(headers)) {
         if (Array.isArray(headers[key])) {
           for (const value of headers[key]) {
-            message += `${key}: ${value}${CRLF}`
+            message += `${key}: ${value}${separator}`
           }
         } else {
-          message += `${key}: ${headers[key]}${CRLF}`
+          message += `${key}: ${headers[key]}${separator}`
         }
       }
     } else {
       return Promise.reject(Error(`"headers" expected to be object or Array, but received ${typeof headers}`))
     }
 
-    return new Promise(function (resolve) {
-      if (reply.raw.socket === null) {
-        resolve()
-        return
-      }
-      reply.raw.socket.write(`${EarlyHints}${message}${CRLF}`, 'ascii', () => {
-      // we do not care the message is sent or lost. Since early hints
-      // is metadata to instruct the clients to do something before actual
-      // content. It should never affect the final result if it lost.
-        resolve()
-      })
-    })
+    return write(this, message)
   }
 
   function writeEarlyHintsLinks (links) {
-    const reply = this
     let message = ''
     for (let i = 0; i < links.length; i++) {
-      message += `${formatEntry(links[i], formatEntryOpts)}${CRLF}`
+      message += `${formatEntry(links[i], formatEntryOpts)}${separator}`
     }
 
-    return new Promise(function (resolve) {
-      if (reply.raw.socket === null) {
-        resolve()
-        return
-      }
-      reply.raw.socket.write(`${EarlyHints}${message}${CRLF}`, 'ascii', () => {
-      // we do not care the message is sent or lost. Since early hints
-      // is metadata to instruct the clients to do something before actual
-      // content. It should never affect the final result if it lost.
+    return write(this, message)
+  }
+
+  const write = http2
+    ? function write (reply, message) {
+      return new Promise(function (resolve) {
+        if (reply.raw.socket === null) {
+          resolve()
+          return
+        }
+        reply.raw.stream.additionalHeaders({
+          ':status': 103,
+          Link: message
+        })
         resolve()
       })
-    })
-  }
+    }
+    : function write (reply, message) {
+      return new Promise(function (resolve) {
+        if (reply.raw.socket === null) {
+          resolve()
+          return
+        }
+        reply.raw.socket.write(`${EarlyHints}${message}${separator}`, 'ascii', () => {
+        // we do not care the message is sent or lost. Since early hints
+        // is metadata to instruct the clients to do something before actual
+        // content. It should never affect the final result if it lost.
+          resolve()
+        })
+      })
+    }
 
   fastify.decorateReply('writeEarlyHints', writeEarlyHints)
 
--- test/http2.test.js
+++ test/http2.test.js
@@ -3,17 +3,33 @@
 const { test } = require('tap')
 const Fastify = require('fastify')
 const eh = require('../index')
+const http2 = require('http2')
 
-test('Should throw when http2 server', async (t) => {
-  t.plan(1)
+test('initial test', (t) => {
+  t.plan(3)
   const fastify = Fastify({ http2: true })
   fastify.register(eh)
   fastify.get('/', async (request, reply) => {
     await reply.writeEarlyHintsLinks([
-      'Link: </style.css>; rel=preload; as=style',
-      'Link: </script.js>; rel=preload; as=script'
+      '</style.css>; rel=preload; as=style',
+      '</script.js>; rel=preload; as=script'
     ])
     return { hello: 'world' }
   })
-  t.rejects(fastify.ready(), 'Early Hints cannot be used with a HTTP2 server.')
+
+  fastify.listen({ port: 0 }, (err, address) => {
+    t.error(err)
+    const client = http2.connect(address)
+    const req = client.request()
+
+    req.on('headers', (header) => {
+      t.equal(header[':status'], 103)
+      t.equal(header.link, '</style.css>; rel=preload; as=style,</script.js>; rel=preload; as=script,')
+      req.end()
+      client.close()
+      fastify.close()
+    })
+
+    req.on('error', err => t.error(err))
+  })
 })
