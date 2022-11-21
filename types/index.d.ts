92d5914f6322cf56614ff968f267ff9b09fecf6c
uzlopak <aras.abbasi@googlemail.com>
2022-11-21 20:03:34 +0100

    make nodenext compatible

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,26 +1,36 @@
+/* eslint no-redeclare: off */
+/* eslint no-use-before-define: off */
+
 import { FastifyPluginCallback } from 'fastify'
 
-type EarlyHintAs = 'document' | 'script' | 'image' | 'style' | 'font';
-type EarlyHintCORS = 'anonymous' | 'use-credentials' | 'crossorigin';
-type EarlyHintRel = 'dns-prefetch' | 'preconnect' | 'prefetch' | 'preload' | 'prerender';
+type FastifyEarlyHints = FastifyPluginCallback<fastifyEarlyHints.EarlyHintPluginOptions>
 
-export interface EarlyHintItem {
-  href: string;
-  rel: EarlyHintRel;
-  cors?: boolean | EarlyHintCORS;
-  as?: EarlyHintAs;
-}
+declare namespace fastifyEarlyHints {
+  type EarlyHintAs = 'document' | 'script' | 'image' | 'style' | 'font';
+  type EarlyHintCORS = 'anonymous' | 'use-credentials' | 'crossorigin';
+  type EarlyHintRel = 'dns-prefetch' | 'preconnect' | 'prefetch' | 'preload' | 'prerender';
 
-export interface EarlyHintPluginOptions {
-  warn?: boolean
+  export interface EarlyHintItem {
+    href: string;
+    rel: EarlyHintRel;
+    cors?: boolean | EarlyHintCORS;
+    as?: EarlyHintAs;
+  }
+
+  export interface EarlyHintPluginOptions {
+    warn?: boolean
+  }
+
+  export const fastifyEarlyHints: FastifyEarlyHints
+  export { fastifyEarlyHints as default }
 }
 
 declare module 'fastify' {
   interface FastifyReply { // eslint-disable-line no-unused-vars
     writeEarlyHints: (headers: Record<string, string | string[]> | { name: string, value: string }[]) => Promise<void>;
-    writeEarlyHintsLinks: (content: string[] | EarlyHintItem[]) => Promise<void>;
+    writeEarlyHintsLinks: (content: string[] | fastifyEarlyHints.EarlyHintItem[]) => Promise<void>;
   }
 }
 
-declare const fastifyCors: FastifyPluginCallback<EarlyHintPluginOptions>
-export default fastifyCors
+declare function fastifyEarlyHints(...params: Parameters<FastifyEarlyHints>): ReturnType<FastifyEarlyHints>
+export = fastifyEarlyHints
